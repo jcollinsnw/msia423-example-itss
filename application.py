9925c6c586bcb65b197377a6e84b3434c6bd23de
Jacob Collins <jacob.collins@northwestern.edu>
2018-03-02 12:24:16 -0600

    Add ability to add cities.

--- application.py
+++ application.py
@@ -1,7 +1,10 @@
 from flask import render_template, request, redirect, url_for, flash
 from msiapp import application, db
 from msiapp.models import Track, City
-
+import requests
+import json
+import os
+import time
 
 @application.route('/')
 def index():
@@ -32,6 +35,32 @@ def add_entry():
 @application.route('/city')
 def city():
     return render_template('city.html', cities = City.query.all())
+
+@application.route('/addCity', methods=['POST'])
+def add_city():
+    """View that process a POST with new song input
+
+    :return: redirect to index page
+    """
+    url = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s" % (
+                request.form['city'],
+                os.environ['MAPS_KEY'])
+
+    response = requests.get(url)
+
+    # debug
+    weather = json.loads(response.text)
+    timestamp = int(time.time())
+    name = weather['name']
+    country = weather['sys']['country']
+    temperature = weather['main']['temp']
+    max_temp = weather['main']['temp_max']
+    min_temp = weather['main']['temp_min']
+    city = City(name=name,timestamp=timestamp,country=country,temperature=temperature,max_temp=max_temp,min_temp=min_temp)
+    db.session.add(city)
+    db.session.commit()
+
+    return redirect(url_for('city'))
 
 if __name__ == "__main__":
     application.run(debug=True)
